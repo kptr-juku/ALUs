bfefa8783fbb0f67eec43de2f79ed14bc49e6bd5
KPTR Tiiger <tiiger.foo@icloud.com>
2026-06-26 15:20:03 +0300

    CUDAARCHs teintroduction and default architecture updates.

--- algs/range-doppler-terrain-correction/src/terrain_correction_kernel.cu
+++ algs/range-doppler-terrain-correction/src/terrain_correction_kernel.cu
@@ -50,7 +50,7 @@ void CalculateVelocitiesAndPositions(const int source_image_height, const double
                                      cuda::KernelArray<snapengine::OrbitStateVectorComputation> vectors,
                                      cuda::KernelArray<snapengine::PosVector> velocities,
                                      cuda::KernelArray<snapengine::PosVector> positions) {
-    dim3 block_dim{1024};
+    dim3 block_dim{256};
     dim3 grid_dim{static_cast<unsigned int>(cuda::GetGridDim(block_dim.x, source_image_height))};
 
     CalculateVelocitiesAndPositionsKernel<<<grid_dim, block_dim>>>(first_line_utc, line_time_interval, vectors,
